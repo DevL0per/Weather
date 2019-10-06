9daa6e5c1bfe85c5cca3d486f07b4c31c3e1d04e
DevL0per <vazhnik.roma@gmail.com>
2019-10-06 09:35:45 +0300

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Роман Важник on 06/10/2019.
+//  Copyright © 2019 Роман Важник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
